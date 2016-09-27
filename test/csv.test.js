f149d733e121274caf73e51596a26e23577313ed
Houfeng <admin@xhou.net>
2016-09-27 18:52:09 +0800

    1.0.6

--- test/csv.test.js
+++ test/csv.test.js
@@ -42,24 +42,34 @@ describe('csv', function () {
     });
 
     it('headline', function () {
-      var list = csv.parse('姓名,年龄\n"z,\"s",20\nls,21', true);
-      assert.deepEqual(list, [{ '姓名': 'z,"s', '年龄': '20' }, { '姓名': 'ls', '年龄': '21' }]);
+      var list = csv.parse('姓名,年龄\n"z,\\"s",20\nls,21', true);
+      assert.deepEqual(list, [{ '姓名': 'z,\\"s', '年龄': 20 }, { '姓名': 'ls', '年龄': 21 }]);
     });
 
     it('columns', function () {
-      var list = csv.parse('"z,\"s",20\nls,21', false, [
+      var list = csv.parse('"z,\\"s",20\nls,21', false, [
         { name: 'name' },
         { name: 'age' }
       ]);
-      assert.deepEqual(list, [{ name: 'z,"s', age: '20' }, { name: 'ls', age: '21' }]);
+      assert.deepEqual(list, [{ name: 'z,\\"s', age: 20 }, { name: 'ls', age: 21 }]);
     });
 
     it('columns & headline', function () {
-      var list = csv.parse('姓名,年龄\n"z,\"s",20\nls,21', true, [
+      var list = csv.parse('姓名,年龄\n"z,\\"s",20\nls,21', true, [
         { name: 'name', displayName: '姓名' },
         { name: 'age', displayName: '年龄' }
       ]);
-      assert.deepEqual(list, [{ name: 'z,"s', age: '20' }, { name: 'ls', age: '21' }]);
+      assert.deepEqual(list, [{ name: 'z,\\"s', age: 20 }, { name: 'ls', age: 21 }]);
+    });
+
+  });
+
+
+  describe('parseLine()', function () {
+
+    it('case1', function () {
+      var rs = csv.parseLine('1,"a\\",b",2,"c,d",true,"true"');
+      assert.deepEqual(rs, [1, 'a\\",b', 2, 'c,d', true, 'true']);
     });
 
   });
